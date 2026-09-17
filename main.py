25349ed36dcd33402a4aaeed3b61c8b52e2d5fa1
alloy01 <ajeettsingh51@gmail.com>
2026-09-17 21:12:47 +0530

    refactor: data is stored in a .json file instead of txt file.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import string
 import random
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 
@@ -12,7 +13,7 @@ args = parser.parse_args()
 def gen_id(length: int) -> str:
     salt = string.ascii_letters + string.digits
     alpha_numeric = ''.join(random.choices(salt, k=length))
-    return f'{alpha_numeric}'
+    return alpha_numeric
 
 duped_id = True
 
@@ -21,18 +22,16 @@ short_id = ''
 while duped_id == True:
     proto_id = gen_id(args.length)
 
-    with open('data.txt', 'r') as file:
-        for line in file:
-            clean_line = line.strip()
-            stored_id = clean_line.split('-->')[1]
-
-            if stored_id == proto_id:
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+        for line in data:
+            if line == proto_id:
                 break
-
         else:
             duped_id = False
             short_id = proto_id
 
+data[short_id] = args.url
 
-with open('data.txt', 'a') as file:
-    file.write(f'{args.url} --> {short_id}\n')
+with open('data.json', 'w') as file:
+    json.dump(data, file, indent=4)
