1545c0ab8c591cdd0e9ead5f78378e083c0a72e0
alloy01 <ajeettsingh51@gmail.com>
2026-09-19 19:28:43 +0530

    feat: added a data loaded with SQLite.

--- database.py
+++ database.py
@@ -17,3 +17,32 @@ def connect_db():
     conn.close()
 
     print("connection intialized.")
+
+def push_data(code: str, url: str):
+    conn = sqlite3.connect("data.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO code_link (code, link) VALUES(
+            (?, ?)
+        )
+    """, (code, url))
+
+    conn.commit()
+    conn.close()
+
+    print("data pushed.")
+
+def load_data():
+    conn = sqlite3.connect("data.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM code_link")
+    print(cursor.fetchall)
+
+    conn.commit()
+    conn.close()
+
+    print("data loaded.")
